a803c6905d89fc970bbca15b383916b42b3f87f2
Khairan Aja <khairking6@gmail.com>
2026-04-24 15:28:29 +0800

    fix: use named path parameter for Express 5 catch-all route

--- server.js
+++ server.js
@@ -142,7 +142,7 @@ app.listen(PORT, '0.0.0.0', () => {
 
 // Catch-all route for frontend
 if (fs.existsSync(join(distPath, 'index.html'))) {
-    app.get('/(.*)', (req, res) => {
+    app.get('/:path*', (req, res) => {
         res.sendFile(join(distPath, 'index.html'));
     });
 }
