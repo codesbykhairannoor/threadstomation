9cc5139e8e6078f2f0763ab191dbbb0af5e1498b
Khairan Aja <khairking6@gmail.com>
2026-06-09 19:07:08 +0800

    cleanup: remove test files and scratch directory - unneeded  files

--- scratch/list-accounts.mjs
+++
@@ -1,8 +0,0 @@
-import sql from '../lib/database.js';
-
-async function listAccounts() {
-  const res = await sql`SELECT id, name, master_prompt FROM accounts`;
-  console.log(res);
-  process.exit(0);
-}
-listAccounts();
--- test-server.mjs
+++
@@ -1,2 +0,0 @@
-import app from './api/instagram.mjs';
-app.listen(3001, () => console.log('Server running on 3001'));
